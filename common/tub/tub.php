6448f0c977ee0ce01aa4e943453e75ba8f5df95d
gredchilipepperofmarine-create <g.redchilipepper.ofmarine@gmail.com>
2026-09-14 13:54:19 +0900

    デザイン調整

--- common/tub/tub.php
+++ common/tub/tub.php
@@ -4,49 +4,48 @@
       <h2 class="techArea">Tech Notes</h2>
       <div class="listArea">
         <ul class="tubs">
-          <li><button type="button" class="tubBtn isOpen" data-btn=".note1">Note1</button></li>
-          <li><button type="button" class="tubBtn" data-btn=".cssArea">Note2</button></li>
-          <li><button type="button" class="tubBtn" data-btn=".jsArea">Note3</button></li>
-          <li><button type="button" class="tubBtn" data-btn=".phpArea">Note4</button></li>
+          <li><button type="button" class="tubBtn isOpen" data-btn=".note1">背景画像</button></li>
+          <li><button type="button" class="tubBtn" data-btn=".cssArea">UFO</button></li>
+          <!-- <li><button type="button" class="tubBtn" data-btn=".jsArea">Note3</button></li> -->
+          <li><button type="button" class="tubBtn" data-btn=".phpArea">サイト全体</button></li>
         </ul>
       </div>
     </div>
     <div class="col-12 tub note1">
       <div class="tubInner">
         <ul class="skills">
-          <li>Bootstrapを使用してベース作成</li>
-          <li></li>
-          <li></li>
+          <li>Javascriptで自動生成。発生位置や画像の大きさ、流れるスピードなどランダムに設定</li>
+          <li>メモリ負担にならないよう画面外で削除</li>
+          <li>footer部分はCSSアニメーションのみ。万が一流れる画像が生成されなくても、RPG独特の冒険感を表現</li>
         </ul>
       </div>
     </div>
     <div class="col-12 tub cssArea isHide">
       <div class="tubInner">
         <ul class="skills">
-          <li>フッターエリアのアニメーションと</li>
+          <li>ランダムな移動はJavascriptとCSSアニメーションで制御</li>
           <li>その他装飾で活用</li>
           <li></li>
           <li></li>
         </ul>
       </div>
     </div>
-    <div class="col-12 tub jsArea isHide">
+    <!-- <div class="col-12 tub jsArea isHide">
       <div class="tubInner">
         <ul class="skills">
           <li>ローディング画面</li>
           <li>背景画像の自動生成</li>
           <li>トップへ戻るボタンの発生制御</li>
-          <li>タブの作成</li>
         </ul>
       </div>
-    </div>
+    </div> -->
     <div class="col-12 tub phpArea isHide">
       <div class="tubInner">
         <ul class="skills">
+          <li>コードはマジックナンバーを排除し保守性を向上</li>
+          <li>Webアクセシビリティの向上のため、画像クリックが必要なローディング画面では5秒で自動遷移する仕組みを実装</li>
           <li></li>
           <li></li>
-          <li></li>
-          <li>PHP</li>
         </ul>
       </div>
     </div>
--- includes/potfolio.sql
+++ includes/potfolio.sql
@@ -40,8 +40,8 @@ CREATE TABLE main_creates (
 
 INSERT INTO `main_creates`(`id`, `category`, `title`, `repo_name`, `description`, `creativity`, `note1`, `note2`, `dev_time`, `root_pass`) VALUES 
 (1,'Game', 'RPG Battle System', 'RPG', 'Javascript演習開始直後の自主制作ゲーム1。\nエンカウントと戦闘ロジックを実装', '戦闘画面を基礎的なJavascriptで記述後、STGのロジックを応用してキャラクターの移動とエンカウント判定に使用', '※制作補助としてAIを使用', '※戦闘画面はAIでデザイン', '25', '../RPG/RPG/index.html'),
-(2,'Game','Shooting Game', 'STG', 'Javascript演習開始直後の自主制作ゲーム2', 'Canvasを用いた描画処理の軽量化と、PHP演習後にスコア保持ロジックを実装', '※制作補助としてAIを使用', '', '20','../STG/index.html'),
+(2,'Game','Shooting Game', 'STG', 'Javascript演習開始直後の自主制作ゲーム2', 'Canvasを用いた描画処理の軽量化と、PHP演習後にスコア保持ロジックを実装', '※制作補助としてAIを使用', '', '20','http://www.gredchilipepper.shop/creates/STG/index.php'),
 (3,'デモHP', 'How To Drive?', 'HowToDrive', '演習開始直後の自主制作サイト', 'Web制作学習の初期段階で、基本タグのレイアウト検証用に制作', '※レスポンシブ未対応', '', '15', '../HowToDrive/index.html'),
 (4,'デモHP', '新作映画', 'Movie', '演習初期の自主制作サイト', '映画告知風のデザインと内容を0から作成。CSS装飾の練習として制作', '※レスポンシブ未対応', '', '15', '../movie/index.html'),
 (5,'デモHP', 'Design House Renovation', 'DesignHouseRenovation', 'HTML/CSS個人製作課題', 'Flexbox/Gridを用いたレスポンシブ配置と、カンプファイルに忠実なデザインの再現', '', '', '50', '../DesignHouseRenovation/index.html'),
-(6,'デモHP', 'C.C.Donuts', 'ccdonuts', 'PHP個人製作課題', 'EC風ショッピングサイト機能の構築', '※制作補助としてAIを使用', '', '80', '../ccdonuts/index.php');
+(6,'デモHP', 'C.C.Donuts', 'ccdonuts', 'PHP個人製作課題', 'EC風ショッピングサイト機能の構築', '※制作補助としてAIを使用', '', '80', 'http://www.gredchilipepper.shop/creates/ccdonuts/index.php');
