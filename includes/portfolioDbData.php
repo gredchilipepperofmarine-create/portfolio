2a27d1f44cb5c526f4b893c22014b3ccf20fcb6d
gredchilipepperofmarine-create <g.redchilipepper.ofmarine@gmail.com>
2026-09-18 15:49:22 +0900

    Delete includes/portfolioDbData.php

--- includes/portfolioDbData.php
+++
@@ -1,6 +0,0 @@
-<?php
-// データベース接続設定(ufoCount専用)
-define('ACCESSDB', 'mysql:host=localhost;dbname=portfolio;charset=utf8mb4');
-define('DBID', 'portfolioUser');
-define('DBPW', 'portfolioPassword');
-?>
